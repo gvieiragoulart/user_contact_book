9d2f53c2eca592bf72f738a6761f08556308a6dd
Gabriel Vieira <gabriel.goulart@tradetechnology.com.br>
2023-06-17 20:31:50 -0300

    scribe

--- user-contacts/app/Http/Controllers/AuthController.php
+++ user-contacts/app/Http/Controllers/AuthController.php
@@ -9,20 +9,15 @@ use App\Services\LoginService;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
     }
 
     /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * Faz o login do usuário.
+     * @bodyParam email string required Email do usuário. Example: joao@teste.com
+     * @bodyParam password string required Senha do usuário. Example: 123456
      */
     public function login(LoginRequest $request, LoginService $loginService)
     {
@@ -38,21 +33,11 @@ class AuthController extends Controller
         }
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function me()
     {
         return response()->json(auth()->user());
     }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function logout()
     {
         auth()->logout();
@@ -60,23 +45,11 @@ class AuthController extends Controller
         return response()->json(['message' => 'Successfully logged out']);
     }
 
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function refresh()
     {
         return $this->respondWithToken(auth()->refresh());
     }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     protected function respondWithToken($token)
     {
         return response()->json([
